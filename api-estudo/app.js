960912c1e51bae4abfea47bccbb6bfd84faab68b
Victor  Zanelo <victorzanelo4@gmail.com>
2021-12-20 14:21:51 -0300

    add pastas

---
+++ api-estudo/app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    console.log("chamou api home");
+    res.send('pagina home');
+});
+
+app.get('/contacts', (req, res) => {
+    console.log('chamou api contacts');
+    res.send('pagina contacts');
+});
+
+app.get('/search', (req, res) => {
+    var cpf = req.query.cpf;
+
+    res.send(`cpf igual a ${cpf}`)
+})
+
+app.post('/contacts', (req, res) => {
+    var name = req.body.name;
+    var email = req.body.email;
+    var message = req.body.message;
+
+    var response_message = `Obrigado ${name} pela mensagem.`;
+
+    res.send(response_message);
+})
+
+app.post('/users/:id', (req, res) => {
+    var id = req.params.id;
+
+    res.send(`param = ${id}`);
+})
+
+app.listen(3000, () =>{
+    console.log("servidor na porta 3000");
+});
